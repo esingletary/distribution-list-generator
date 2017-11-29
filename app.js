9315f70eca7e1fd3b22419dfeda97c94af6819da
Emmanuel Singletary <me@manny.co>
2017-11-29 01:13:08 -0500

    Handlebars for views, and an editorconfig

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
-const port = 5000;
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Reports route
+app.get('/reports', (req, res) => {
+  res.render('reports');
+});
+
+const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
