9b3e0e6328cb32c086024bd256b4e28abd7ef3bf
Emmanuel Singletary <me@manny.co>
2017-11-29 02:18:39 -0500

    More views and layouts

--- app.js
+++ app.js
@@ -17,5 +17,15 @@ app.get('/reports', (req, res) => {
   res.render('reports');
 });
 
+// Lists route
+app.get('/lists', (req, res) => {
+  res.render('lists');
+});
+
+// Notes route
+app.get('/notes', (req, res) => {
+  res.render('notes');
+});
+
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
