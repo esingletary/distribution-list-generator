167e19f8eb2ded997181367df28dbf364135ba96
Emmanuel Singletary <me@manny.co>
2017-12-01 23:54:37 -0500

    switch MySQL to pooling to prevent timeout bugs

--- config/database.js
+++ config/database.js
@@ -1,34 +1,38 @@
 const mysql = require('mysql');
 const keys = require('./keys');
 
-const connection = mysql.createConnection(keys.db);
-
-connection.connect((err) => {
-  if (err) {
-    console.log('Database error: ' + err);
-  } else {
-    console.log('Database successfully connected');
-  }
-});
+const pool = mysql.createPool(keys.db);
 
 exports.execQueryWithParams = function(query, values) {
   return new Promise ((resolve, reject) => {
-    connection.query(query, values, (error, results, fields) => {
+    pool.getConnection((err, connection) => {
+      connection.query(query, values, (error, results, fields) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(results);
+      })
       if (error) {
         return reject(error);
       }
-      return resolve(results);
+      connection.release();
     })
   })
 }
 
 exports.execQuery = function(query) {
   return new Promise ((resolve, reject) => {
-    connection.query(query, (error, results, fields) => {
+    pool.getConnection((err, connection) => {
+      connection.query(query, (error, results, fields) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(results);
+      })
       if (error) {
         return reject(error);
       }
-      return resolve(results);
+      connection.release();
     })
   })
 }
